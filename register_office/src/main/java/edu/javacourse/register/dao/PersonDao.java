f3abc4e868ff2bdce34eb5408a0f95034de7ec95
Ivan <volkov19@mail.ru>
2023-02-19 15:43:47 +0400

    123 Spring ORM, change springContext.xml

--- register_office/src/main/java/edu/javacourse/register/dao/PersonDao.java
+++ register_office/src/main/java/edu/javacourse/register/dao/PersonDao.java
@@ -4,17 +4,16 @@ import edu.javacourse.register.domain.Person;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import java.util.List;
 
 public class PersonDao {
+    
+@PersistenceContext    
 private EntityManager entityManager;
 
-   public PersonDao() {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("persistence");
-        entityManager = factory.createEntityManager();
-    }
-
+  
     public List<Person> findPersons() {
         Query query = entityManager.createNamedQuery("Person.findPersons");
         query.setParameter("personId", 1L);
--- register_office/src/main/java/edu/javacourse/register/rest/MarriageController.java
+++ register_office/src/main/java/edu/javacourse/register/rest/MarriageController.java
@@ -6,6 +6,7 @@ import edu.javacourse.register.view.MarriageResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service("controller")
@@ -15,6 +16,7 @@ public class MarriageController {
             .getLogger(MarriageController.class);
     
     @Autowired
+    @Qualifier("marriageService")
     private MarriageManager marriageManager;
 
     public MarriageResponse findMarriageCertificate(MarriageRequest request) {
